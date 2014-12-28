45c14daff7eb82bc8cb19a38783bfe580225a449
Steeve <stephentcannon@gmail.com>
2014-12-28 12:45:52 -0500

    show route route

--- client/Router.js
+++ client/Router.js
@@ -10,8 +10,14 @@ Router.plugin('dataNotFound', {notFoundTemplate: 'notFound'});
 
 Router.plugin('loading', {loadingTemplate: 'loading'});
 
-Router.route('/dashbaord', {
+Router.route('/', {
   name: 'dashboard',
-  path: '/dashboard',
+  path: '/',
   template: 'dashboard',
 });
+
+// Router.route('/dashbaord', {
+//   name: 'dashboard',
+//   path: '/dashboard',
+//   template: 'dashboard',
+// });
